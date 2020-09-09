f3631afdbfef8cbee2f9baf044ff16b14badc0ca
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-09-09 13:20:31 +0300

    Added SignUp.java and LearnMoreActivity.java as well as their layouts

--- app/src/main/java/com/example/e_loan/MainActivity.java
+++ app/src/main/java/com/example/e_loan/MainActivity.java
@@ -39,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this,SignUp.class);
+                startActivity(intent);
             }
         });
     }
